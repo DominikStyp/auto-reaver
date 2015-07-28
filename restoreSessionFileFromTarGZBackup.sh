a607dec9f984ff75571abde18f0f86a52bd8ab5d
root <root@bt.foo.org>
2015-07-28 04:46:28 +0200

    Added new tool restoreSessionFileFromTarGZBackup to restore choosed session file quickly from the .tar.gz backup

---
+++ restoreSessionFileFromTarGZBackup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+source $(pwd)/configurationSettings
+
+if [[ -z "$1" ]]; then
+   echo "Usage: $0 [PATH_TO_BACKUP_FILE] [SESSION_FILE_NAME]"
+   echo "[SESSION_FILE_NAME] EXAMPLE:    AABBCCDDEEFF.wpc"
+   echo "[PATH_TO_BACKUP_FILE] EXAMPLE:  ./reaverBackup/reaverBackup_YYYY-MM-DD_HH-MM-SS.tar.gz"
+   echo "REMEMBER! Reaver session file is taken from ./configurationSettings file (REAVER_SESSION_DIR)."
+   echo "So you have to have correct settings there."
+   exit
+fi
+BACKUP_F=$1
+SESS_FILE=$2
+SESS_DIR=$REAVER_SESSION_DIR # from configurationSettings file
+# session without trailing slash
+SESS_DIR_NO_T_SLASH=$(echo ${SESS_DIR:1}) # substring in bash is ${VAR:fromChar:charsNum}
+
+
+tar -xOf ${BACKUP_F} ${SESS_DIR_NO_T_SLASH}/${SESS_FILE} > ${SESS_DIR}/${SESS_FILE}
+echo "tar -xOf ${BACKUP_F} ${SESS_DIR_NO_T_SLASH}/${SESS_FILE} > ${SESS_DIR}/${SESS_FILE}"
+echo "Restored session file's head: "
+cat ${SESS_DIR}/${SESS_FILE} | head
